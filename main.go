66fa934e6bacdff4d6fc5f366d4cd06bbfd8874f
Sam Hopkinson <s.hopkinson87@gmail.com>
2024-01-04 16:51:17 +0000

    runes

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 	fmt.Println("This is go studies!")
+
+	var intNum int = 10
+	fmt.Println(intNum)
+
+	var hello string = "How many characters am I?"
+
+	fmt.Println(utf8.RuneCountInString(hello))
 }
